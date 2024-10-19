9d44fd4b95dd20abd196cff35b9cbd06ce1dab4d
PAPA IGWE <papaigwe0@gmail.com>
2024-10-19 21:27:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cfonts from 'cfonts';
 
 const { say } = cfonts;
 
-say("ANNA - MD", {
+say("KAMB - EVO", {
   'font': "block",
   'align': "center",
   'colors': ['#ff9900'],
@@ -21,7 +21,7 @@ say("ANNA - MD", {
   'maxLength': '15'
 });
 
-say("QUEEN-ANNA By toge012345._.", {
+say("KAMB_EVO By PAPAIGWE._.", {
   'font': "chrome",
   'align': "center",
   'colors': ["red", "magenta"],
@@ -42,7 +42,7 @@ const sendHtml = (res, page) => {
   res.sendFile(path.join(htmlDir, page + ".html"));
 };
 
-app.get('/', (req, res) => sendHtml(res, "Anna"));
+app.get('/', (req, res) => sendHtml(res, "Kamb"));
 
 app.listen(port, () => {
   console.log(chalk.green(`Server is running on port ${port}`));
@@ -81,7 +81,7 @@ async function start(scriptName) {
     if (exitCode === 0) return;
     fs.watchFile(scriptArgs[0], () => {
       fs.unwatchFile(scriptArgs[0]);
-      start("anna-core.js");
+      start("kamb-core.js");
     });
   });
 
@@ -89,7 +89,7 @@ async function start(scriptName) {
     console.error(chalk.red(`Error: ${err}`));
     childProcess.kill();
     isRunning = false;
-    start('anna-core.js');
+    start('kamb-core.js');
   });
 
   const pluginsDir = path.join(path.dirname(currentScriptPath), "plugins");
@@ -110,17 +110,17 @@ async function start(scriptName) {
   });
 }
 
-start('anna-core.js');
+start('kamb-core.js');
 
 
 process.on("unhandledRejection", () => {
   console.error(chalk.red("Unhandled promise rejection. Bot will restart..."));
-  start("anna-core.js");
+  start("kamb-core.js");
 });
 
 
 process.on("exit", exitCode => {
   console.error(chalk.red(`Exited with code: ${exitCode}`));
   console.error(chalk.red("Bot will restart..."));
-  start("anna-core.js");
+  start("kamb-core.js");
 });

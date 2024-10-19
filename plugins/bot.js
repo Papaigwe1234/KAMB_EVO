508bfb77c609e05f6c85b060a0014b4bd95d8b8f
PAPA IGWE <papaigwe0@gmail.com>
2024-10-19 22:52:00 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/voice/${pickRandom(["anna", "hi", "menu", "test"])}.mp3`
+let av = `./Assets/voice/${pickRandom(["kamb", "hi", "menu", "test"])}.mp3`
 
-m.reply( `Hello ${taguser} Am 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰 creatd by 𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 Need help?  type /help `)
+m.reply( `Hello ${taguser} Am KAMB-EVO creatd by PAPA IGWE Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(ANNA|TOGE INUMAKI)$/i
+handler.customPrefix = /^(KAMB|PAPA IGWE)$/i
 handler.command = new RegExp
 
 export default handler
